df1b626dc4082649109176dcb7e22a8fa93e483e
jlilly <johannlilly@users.noreply.github.com>
2018-02-23 19:49:52 -0500

    02_03: Create object reducers; ternary if-statements

--- exercise-files/Ch02/02_03/start/src/store/reducers.js
+++ exercise-files/Ch02/02_03/start/src/store/reducers.js
@@ -1,21 +1,13 @@
 import C from '../constants'
 
-export const goal = (state=10, action) => {
+export const goal = (state=10, action) =>
 
-   if (action.type === C.SET_GOAL) {
-   	   return parseInt(action.payload)
-   } else {
-       return state
-   }
+	(action.type === C.SET_GOAL) ?
+   	    parseInt(action.payload) :
+		state
 
-}
+export const skiDay = (state=null, action) =>
 
-export const skiDay = (state=null, action) => {
-
-	if (action.type === C.ADD_DAY) {
-		return action.payload
-	} else {
-		return state
-	}
-
-}
+	(action.type === C.ADD_DAY) ?
+		action.payload :
+		state
