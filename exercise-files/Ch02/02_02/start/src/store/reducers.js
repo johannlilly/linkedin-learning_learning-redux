9454550a8e65fc4be087ca3f8e9f48be14a1f039
jlilly <johannlilly@users.noreply.github.com>
2018-02-23 19:30:06 -0500

    02_02: Build your first reducer; set default parameters for state

--- exercise-files/Ch02/02_02/start/src/store/reducers.js
+++ exercise-files/Ch02/02_02/start/src/store/reducers.js
@@ -4,7 +4,7 @@ import C from '../constants'
 
 // function goal(state, action) {
 // this prevents us from changing the function by using the const keyword
-export const goal = (state, action) => {
+export const goal = (state=10, action) => {
 
 	if (action.type === C.SET_GOAL) {
 		// return the new goal, which is in action.payload
