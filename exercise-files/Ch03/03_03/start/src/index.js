48a8899bbd2bf2e5f8f5c157ddb8cf6794a0366a
jlilly <johannlilly@users.noreply.github.com>
2018-06-05 18:41:01 -0400

    Subscribe to the store

--- exercise-files/Ch03/03_03/start/src/index.js
+++ exercise-files/Ch03/03_03/start/src/index.js
@@ -1,18 +1,18 @@
 import C from './constants'
 import appReducer from './store/reducers'
-// import initialState from './initialState.json'
 import { createStore } from 'redux'
+
+const initialState = (localStorage['redux-store']) ?
+	JSON.parse(localStorage['redux-store']) :
+	{}
 
 const store = createStore(appReducer)
 
-store.subscribe(() => console.log(store.getState()))
-// store has a subscribe method which allows you to subscribe callback handlers that are invoked every time state changes
-store.dispatch({
-	type: C.ADD_DAY,
-	payload: {
-		"resort": "Mt Shasta",
-		"date": "2016-10-28",
-		"powder": false,
-		"backcountry": true
-	}
+window.store = store
+
+store.subscribe(() => {
+
+	const state = JSON.stringify(store.getState())
+	localStorage['redux-store'] = state
+
 })
