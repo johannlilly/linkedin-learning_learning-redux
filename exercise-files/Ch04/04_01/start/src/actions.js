ee5109b7c6f85b6e73cb56a66d504a0135f938ed
jlilly <johannlilly@users.noreply.github.com>
2018-06-06 12:12:34 -0400

    adding application logic outside of reducers

--- exercise-files/Ch04/04_01/start/src/actions.js
+++ exercise-files/Ch04/04_01/start/src/actions.js
@@ -1,6 +1,10 @@
 import C from './constants'
 
 export function addDay(resort, date, powder=false, backcountry=false) {
+
+	// add application logic here
+	// i.e. any functions you don't want to add (or are not supposed to add) in reducers
+
 	// return an action object
 	return {
 		type: C.ADD_DAY,
