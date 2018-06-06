de2c27ab89dead0889e368aeab49cc9212f1594d
jlilly <johannlilly@users.noreply.github.com>
2018-06-06 12:11:38 -0400

    invoke action creator, but some variables are set by default
    handle within the function

--- exercise-files/Ch04/04_01/start/src/actions.js
+++ exercise-files/Ch04/04_01/start/src/actions.js
@@ -1,1 +1,9 @@
 import C from './constants'
+
+export function addDay(resort, date, powder=false, backcountry=false) {
+	// return an action object
+	return {
+		type: C.ADD_DAY,
+		payload: {resort, date, powder, backcountry}
+	}
+}
