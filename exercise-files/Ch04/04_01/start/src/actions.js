01d3ae704887d34bdb1168ed7bd50b295f995ce8
jlilly <johannlilly@users.noreply.github.com>
2018-06-06 12:27:55 -0400

    Create more action creators, declared differently

--- exercise-files/Ch04/04_01/start/src/actions.js
+++ exercise-files/Ch04/04_01/start/src/actions.js
@@ -1,5 +1,9 @@
 import C from './constants'
 
+// 3 functions
+// all are action creators
+// they all do the same thing even though we have declared them differently
+
 export function addDay(resort, date, powder=false, backcountry=false) {
 
 	// add application logic here
@@ -11,3 +15,20 @@ export function addDay(resort, date, powder=false, backcountry=false) {
 		payload: {resort, date, powder, backcountry}
 	}
 }
+
+// create a new action creator for removing a day
+
+export const removeDay = function(date) {
+	// return a new action of type REMOVE_DAY
+	return {
+		type: C.REMOVE_DAY,
+		payload: date
+	}
+}
+
+export const setGoal = (goal) => 
+// use parentheses to return an object
+	({
+		type: C.SET_GOAL,
+		payload: goal
+	})
