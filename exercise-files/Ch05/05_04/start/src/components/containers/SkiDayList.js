7f4728cd8336c680a8a2f3a9dfd79833185daf29
jlilly <johannlilly@users.noreply.github.com>
2018-06-09 14:07:31 -0400

    Map router params to React components
    we are listing the ski days, that the filter property is being received, and when we remove a day it affects our overall state, so it instantaneously also affects the state that's being presented on the home page.

--- exercise-files/Ch05/05_04/start/src/components/containers/SkiDayList.js
+++ exercise-files/Ch05/05_04/start/src/components/containers/SkiDayList.js
@@ -1,27 +1,33 @@
 import SkiDayList from '../ui/SkiDayList'
+import { connect } from 'react-redux'
+import { removeDay } from '../../actions'
 
-const sample = [
-    {
-    "resort": "Stowe",
-    "date": "2017-1-28",
-    "powder": false,
-    "backcountry": false
-  },
-  {
-    "resort": "Tuckerman's Ravine",
-    "date": "2017-1-31",
-    "powder": false,
-    "backcountry": true
-  },
-  {
-    "resort": "Mad River Glen",
-    "date": "2017-2-12",
-    "powder": true,
-    "backcountry": false
-  }
-]
+// can also take any props from parent
+const mapStateToProps = (state, props) =>
+  // return object
+  ({
+    days: state.allSkiDays,
+    // props are provided from the router
+    filter: props.params.filter
+  })
 
+const mapDispatchToProps = dispatch =>
+  // return an object that actually maps function properties to my dispatch function
+  ({
+    onRemoveDay(date) {
+      dispatch(
+        removeDay(date)
+      )
+    }
+  })
+
+export default connect(mapStateToProps, mapDispatchToProps)(SkiDayList)
+
+// pass from parent to container component
+// we pass to UI component
+/*
 export default (props) =>
     <SkiDayList days={sample}
                 filter={props.params.filter}
                 onRemoveDay={date => console.log('remove day on', date)} />
+*/
