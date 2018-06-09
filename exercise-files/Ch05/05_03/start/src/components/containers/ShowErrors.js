eb2eebf6f5afd2b7b00bd25840a0c78b78156116
jlilly <johannlilly@users.noreply.github.com>
2018-06-09 13:43:28 -0400

    Map dispatch to React components
    we have created a container component using the connect function that will map the state of errors to the ShowErrors, and it will also map the dispatch function to any onClearError events.

--- exercise-files/Ch05/05_03/start/src/components/containers/ShowErrors.js
+++ exercise-files/Ch05/05_03/start/src/components/containers/ShowErrors.js
@@ -1,5 +1,35 @@
 import ShowErrors from '../ui/ShowErrors'
+import { clearError } from '../../actions'
+import { connect } from 'react-redux'
 
+// OLD
+/*
 export default () =>
 	<ShowErrors errors={['sample error']}
 						  onClearError={index => console.log('todo: clear error at', index)} />
+*/
+
+// need to map state of errors to the error property
+// also need to dispatch a clearError event when the user clicks the 'x'
+const mapStateToProps = state => {
+
+	// return object that contains keys for each of the properties of the ShowErrors component
+	// when state loads, if there are any errors, they will be passed via the error property
+	return {
+		errors: state.errors
+	}
+} 
+
+// map dispatch function to these component properties
+const mapDispatchToProps = dispatch => {
+	return {
+		onClearError(index) {
+			// also invokes this function
+			dispatch(
+				clearError(index)
+			)
+		}
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowErrors)
