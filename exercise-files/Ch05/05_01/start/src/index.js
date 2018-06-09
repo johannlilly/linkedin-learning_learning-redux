d346fa9e0b99662adb901d377ec41c18431076fc
jlilly <johannlilly@users.noreply.github.com>
2018-06-09 12:39:58 -0400

    incorporate store

--- exercise-files/Ch05/05_01/start/src/index.js
+++ exercise-files/Ch05/05_01/start/src/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import routes from './routes'
 import sampleData from './initialState'
+import storeFactory from './store'
 
 const initialState = (localStorage["redux-store"]) ?
     JSON.parse(localStorage["redux-store"]) :
@@ -11,7 +12,11 @@ const initialState = (localStorage["redux-store"]) ?
 const saveState = () => 
     localStorage["redux-store"] = JSON.stringify(store.getState())
 
+const store = storeFactory(initialState)
+store.subscribe(saveState)
+
 window.React = React
+window.store = store
 
 render(
 	routes,
