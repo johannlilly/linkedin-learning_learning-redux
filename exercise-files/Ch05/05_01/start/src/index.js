beeec7d3535e20f42f09140507121c752624c7fa
jlilly <johannlilly@users.noreply.github.com>
2018-06-09 12:42:12 -0400

    incorporate provider, put routes in context

--- exercise-files/Ch05/05_01/start/src/index.js
+++ exercise-files/Ch05/05_01/start/src/index.js
@@ -4,6 +4,8 @@ import { render } from 'react-dom'
 import routes from './routes'
 import sampleData from './initialState'
 import storeFactory from './store'
+// Provider is a component you can wrap around any component tree and it will place the store in context. Context is a feature that will allow any child react component to interact with the store if they want to
+import { Provider } from 'react-redux'
 
 const initialState = (localStorage["redux-store"]) ?
     JSON.parse(localStorage["redux-store"]) :
@@ -19,6 +21,8 @@ window.React = React
 window.store = store
 
 render(
-	routes,
+	<Provider store={store}
+		{routes}
+	</Provider>,
   document.getElementById('react-container')
 )
